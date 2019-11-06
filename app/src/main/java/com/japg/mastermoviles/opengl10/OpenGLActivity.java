0928704be075c9193691d7545708dd402feec26c
sergio <djpep.dj@gmail.com>
2019-11-06 22:21:49 +0100

    Delete unused code and layouts

--- app/src/main/java/com/japg/mastermoviles/opengl10/OpenGLActivity.java
+++ app/src/main/java/com/japg/mastermoviles/opengl10/OpenGLActivity.java
@@ -17,108 +17,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class OpenGLActivity extends AppCompatActivity {
-	private GLSurfaceView glSurfaceView;
-	private boolean rendererSet = false;
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-		/*super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
-		*/
-
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         glSurfaceView = new GLSurfaceView(this);
         final OpenGLRenderer openGLRenderer = new OpenGLRenderer(this);
         final ActivityManager activityManager =
-        		(ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         final ConfigurationInfo configurationInfo =
-        		activityManager.getDeviceConfigurationInfo();
+                activityManager.getDeviceConfigurationInfo();
         //final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
         final boolean supportsEs2 =
-        		configurationInfo.reqGlEsVersion >= 0x20000
-        		|| (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-        		&& (Build.FINGERPRINT.startsWith("generic")
-        		|| Build.FINGERPRINT.startsWith("unknown")
-        		|| Build.MODEL.contains("google_sdk")
-        		|| Build.MODEL.contains("Emulator")
-        		|| Build.MODEL.contains("Android SDK built for x86")));
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
         if (supportsEs2) {
-        	// Request an OpenGL ES 2.0 compatible context.
-        	glSurfaceView.setEGLContextClientVersion(2);
-        	// Para que funcione en el emulador
-        	glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-        	// Asigna nuestro renderer.
-        	glSurfaceView.setRenderer(openGLRenderer);
-        	rendererSet = true;
-        	Toast.makeText(this, "OpenGL ES 2.0 soportado",
-        			Toast.LENGTH_LONG).show();
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+            // Para que funcione en el emulador
+            glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+            // Asigna nuestro renderer.
+            glSurfaceView.setRenderer(openGLRenderer);
+            rendererSet = true;
+            Toast.makeText(this, "OpenGL ES 2.0 soportado",
+                    Toast.LENGTH_LONG).show();
         } else {
-        	Toast.makeText(this, "Este dispositivo no soporta OpenGL ES 2.0",
-        			Toast.LENGTH_LONG).show();
-        	return;
+            Toast.makeText(this, "Este dispositivo no soporta OpenGL ES 2.0",
+                    Toast.LENGTH_LONG).show();
+            return;
         }
-        
+
         glSurfaceView.setOnTouchListener(new OnTouchListener() {
-        	@Override
-        	public boolean onTouch(View v, MotionEvent event) {
-        	  if (event != null) {
-        		// Convert touch coordinates into normalized device
-        		// coordinates, keeping in mind that Android's Y
-        		// coordinates are inverted.
-        		final float normalizedX =   (event.getX() / (float) v.getWidth()) * 2 - 1;
-        		final float normalizedY = -((event.getY() / (float) v.getHeight()) * 2 - 1);
-        		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-        			glSurfaceView.queueEvent(new Runnable() {
-        				@Override
-        				public void run() {
-        					openGLRenderer.handleTouchPress(normalizedX, normalizedY);
-        				}
-        			});
-        		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-        			glSurfaceView.queueEvent(new Runnable() {
-        				@Override
-        				public void run() {
-        					openGLRenderer.handleTouchDrag(normalizedX, normalizedY);
-        				}
-        			});
-        		}
-        		return true;
-        	  } else {
-        		return false;
-        	  }
-        	}
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Convert touch coordinates into normalized device
+                // coordinates, keeping in mind that Android's Y
+                // coordinates are inverted.
+                final float normalizedX = (event.getX() / (float) v.getWidth()) * 2 - 1;
+                final float normalizedY = -((event.getY() / (float) v.getHeight()) * 2 - 1);
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    glSurfaceView.queueEvent(new Runnable() {
+                        @Override
+                        public void run() {
+                            openGLRenderer.handleTouchPress(normalizedX, normalizedY);
+                        }
+                    });
+                } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    glSurfaceView.queueEvent(new Runnable() {
+                        @Override
+                        public void run() {
+                            openGLRenderer.handleTouchDrag(normalizedX, normalizedY);
+                        }
+                    });
+                }
+                return true;
+            }
         });
         setContentView(glSurfaceView);
     }
 
     @Override
     protected void onPause() {
-    	super.onPause();
-    	if (rendererSet) {
-    		glSurfaceView.onPause();
-    	}
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
     }
-    		
+
     @Override
     protected void onResume() {
-    	super.onResume();
-    	if (rendererSet) {
-    		glSurfaceView.onResume();
-    	}
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -137,5 +118,5 @@ public class OpenGLActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-   
+
 }
